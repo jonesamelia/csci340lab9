7d11508b06d09be58ddbdba5279958bc4b975274
Amelia Jones <jonesas@hendrix.edu>
2023-11-01 21:04:07 -0500

    Filtering

--- AmeliaUniversity/Pages/Students/Index.cshtml.cs
+++ AmeliaUniversity/Pages/Students/Index.cshtml.cs
@@ -12,21 +12,61 @@ namespace AmeliaUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly AmeliaUniversity.Data.SchoolContext _context;
-
-        public IndexModel(AmeliaUniversity.Data.SchoolContext context)
+        private readonly SchoolContext _context;
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; } = default!;
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
 
-        public async Task OnGetAsync()
+        public string AgeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
-            if (_context.Students != null)
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            CurrentFilter = searchString;
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                            select s;
+
+            if (!String.IsNullOrEmpty(searchString))
             {
-                Student = await _context.Students.ToListAsync();
+                studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+                                    || s.FirstMidName.Contains(searchString));
             }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Age":
+                    studentsIQ = studentsIQ.OrderBy(s => s.Age);
+                    break;
+                case "age_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.Age);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
