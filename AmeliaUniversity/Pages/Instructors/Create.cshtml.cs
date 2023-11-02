8996298cdfe3dc42a705a9ade54f7ea41ac739ac
Amelia Jones <jonesas@hendrix.edu>
2023-11-02 01:08:09 -0500

    Part 6

---
+++ AmeliaUniversity/Pages/Instructors/Create.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AmeliaUniversity.Data;
+using AmeliaUniversity.Models;
+
+namespace AmeliaUniversity.Pages.Instructors
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AmeliaUniversity.Data.SchoolContext _context;
+
+        public CreateModel(AmeliaUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Instructor Instructor { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Instructors.Add(Instructor);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
