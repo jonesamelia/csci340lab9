e2ae34d46d550b2136f9d1e8a356b999cc06e4ab
Amelia Jones <jonesas@hendrix.edu>
2023-11-01 22:38:54 -0500

    Part 3

--- AmeliaUniversity/Pages/Students/Index.cshtml.cs
+++ AmeliaUniversity/Pages/Students/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using AmeliaUniversity.Models;
 using AmeliaUniversity.Data;
 
@@ -13,9 +14,12 @@ namespace AmeliaUniversity.Pages.Students
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-        public IndexModel(SchoolContext context)
+        private readonly IConfiguration Configuration;
+
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
@@ -25,14 +29,24 @@ namespace AmeliaUniversity.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
             // using System;
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
             AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             CurrentFilter = searchString;
 
@@ -66,7 +80,9 @@ namespace AmeliaUniversity.Pages.Students
                     break;
             }
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(
+            studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
